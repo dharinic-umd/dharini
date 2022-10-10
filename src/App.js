650c3d09bea9e9323ca741bc54a756f19c6e6c75
Dharini C <dharinic@Dharinis-Air.fios-router.home>
2022-10-10 15:00:25 -0400

    used CSS Tailwind

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header> */}
+
+      <div className="h-14 bg-gradient-to-r from-sky-500 to-indigo-500">
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      React App assignment - Dharini's page
+      <br></br>
+      <br></br>
+      <br></br>
+      <div class="bg-fixed"  style={{ backgroundImage: "url(https://dharinichandrashekar.files.wordpress.com/2021/11/snapseed1.jpg?strip=info&w=2000)" }}>
+          <div className="h-screen"></div>
+      </div>
+      <div class="h-14 bg-gradient-to-r from-sky-500 to-indigo-500"></div>
     </div>
   );
 }
